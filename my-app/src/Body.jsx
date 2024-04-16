1087999f4724830e71beb1786e7352e00f7eb64e
Mariejuhl1 <mani523@student.sdu.dk>
2024-04-16 12:15:37 +0200

    Body underline

--- my-app/src/Body.jsx
+++ my-app/src/Body.jsx
@@ -3,9 +3,9 @@ import "./Body.css";
 function Body() {
   return (
     <div className="Body">
-      <Box title="Produkter" link="/produkter" image="https://fastly.picsum.photos/id/570/200/400.jpg?hmac=Ka2C3Y0V0765SMQsKXw6d1-L7PKIMvluEQsO8EEL538"></Box>
+      <Box title="Products" link="/produkter" image="https://fastly.picsum.photos/id/570/200/400.jpg?hmac=Ka2C3Y0V0765SMQsKXw6d1-L7PKIMvluEQsO8EEL538"></Box>
       <Box title="Guides" link="/guides" image="https://fastly.picsum.photos/id/570/200/400.jpg?hmac=Ka2C3Y0V0765SMQsKXw6d1-L7PKIMvluEQsO8EEL538"></Box>
-      <Box title="Om os" link="/om-os" image="https://fastly.picsum.photos/id/570/200/400.jpg?hmac=Ka2C3Y0V0765SMQsKXw6d1-L7PKIMvluEQsO8EEL538"></Box>
+      <Box title="About Arne" link="/om-os" image="https://fastly.picsum.photos/id/570/200/400.jpg?hmac=Ka2C3Y0V0765SMQsKXw6d1-L7PKIMvluEQsO8EEL538"></Box>
     </div>
   );
 }
